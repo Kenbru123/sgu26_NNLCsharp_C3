aca73e51bcf3cb4e5bff3a96af9f608c0841f731
ThanhDaiDz <daideptrai555@gmail.com>
2026-09-16 11:10:16 +0700

    Tuan01/Buoi1/lam bai

---
+++ Tuan01/Buoi1/bai1.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("******************************"); 
+Console.WriteLine("** Truong: Dai Hoc Sai Gon  **");
+Console.WriteLine("** Khoa: CNTT               **");
+Console.WriteLine("** Ho ten: Nguyen Thanh Dai **");
+Console.WriteLine("******************************"); 
