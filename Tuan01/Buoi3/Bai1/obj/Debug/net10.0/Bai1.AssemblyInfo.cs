c952ddc63c5a50f21f7471c3b38ee8b71b85e6eb
ThanhDaiDz <daideptrai555@gmail.com>
2026-09-19 23:55:00 +0700

    Tuan01/Lab01/lam btap co ban

--- Tuan01/Buoi3/Bai1/obj/Debug/net10.0/Bai1.AssemblyInfo.cs
+++ Tuan01/Buoi3/Bai1/obj/Debug/net10.0/Bai1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fb57f2f734f2254a232f4edd30c0be09ab53912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de721d71ab5b37340cb1f4b33bac8224a9b1992a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tuan01/buoi7,8,9_Đăng Tiến/readme.md
+++
@@ -1,4 +0,0 @@
-Lê Đăng Tiến _3124411311
-lớp DCT124C3
-thực hành buổi 7,8,9 - lab01
-môn ngôn ngữ lập trình CSharp
