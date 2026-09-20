b0c943cadb6a0137581399ff10bce3dcc638a59a
ThanhDaiDz <daideptrai555@gmail.com>
2026-09-20 10:44:01 +0700

    Tuan01/Lab01/lam btap co ban

--- Tuan01/Buoi11/Bai2/obj/Debug/net10.0/BT2Prj.AssemblyInfo.cs
+++ Tuan01/Buoi11/Bai2/obj/Debug/net10.0/BT2Prj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT2Prj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c952ddc63c5a50f21f7471c3b38ee8b71b85e6eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3bf7127258dd3a1d2896b2ce461b068b284d419")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT2Prj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT2Prj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Tuan01/Readme.md
@@ -0,0 +1,4 @@
+# Nguyễn Xuân Hậu Thành 3124411278
+# Lưu Lương Mạnh Dũng 3124411053
+# Lê Đăng Tiến 3124411311
+# Nguyễn Thành Đại 3124411061
